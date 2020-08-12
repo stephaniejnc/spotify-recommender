75b6e173e963cba75a35f218729fb431dcda1805
chiujess <chiujess@umich.edu>
2020-08-12 12:49:58 -0400

    Add playlist object to datastore

--- datastore.js
+++ datastore.js
@@ -11,22 +11,26 @@ async function quickStart() {
   });
 
   // The kind for the new entity
-  const kind = 'Task';
+  const kind = 'Playlist';
   // The name/ID for the new entity
-  const name = 'sampletask1';
+  const name = 'username_playlistname';
   // The Cloud Datastore key for the new entity
-  const taskKey = datastore.key([kind, name]);
+  const playlistKey = datastore.key([kind, name]);
 
   // Prepares the new entity
-  const task = {
-    key: taskKey,
+  const playlist = {
+    key: playlistKey,
     data: {
-      description: 'Buy milk',
+      spotify_id: 'spotify_id',
+      owner: 'owner',
+      playlist_name: 'playlist_name',
+      tracks: ['track1', 'track2'],
+      url: 'placeholder-url',
     },
   };
 
   // Saves the entity
-  await datastore.save(task);
-  console.log(`Saved ${task.key.name}: ${task.data.description}`);
+  await datastore.save(playlist);
+  console.log(`Saved ${playlist.key.name}: ${playlist.data.owner} playlist`);
 }
 quickStart().catch(console.error);
