14d52c517e5567ac24e05f65e6721a910ededc75
Jessica Feng <jyf5@cornell.edu>
2020-08-24 15:33:01 -0700

    Add structuring of getting recommendations

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 
-const indexRouter = require('./routes/index')
+const indexRouter = require('./routes/index');
+const playlistRouter = require('./routes/playlist');
+require('dotenv').config();
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
@@ -13,6 +15,7 @@ app.use(express.static('public'))
 app.use(express.static(__dirname + '/public'))
 
 app.use('/', indexRouter)
+app.use('/playlist/', playlistRouter);
 
 
 /**
@@ -29,8 +32,8 @@ var cors = require('cors');
 var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
 
-var client_id = process.env.client_id; // Your client id
-var client_secret = process.env.client_secret; // Your secret
+var client_id = process.env.CLIENT_ID; // Your client id
+var client_secret = process.env.CLIENT_SECRET; // Your secret
 var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
 
 /**
@@ -157,113 +160,7 @@ app.get('/refresh_token', function (req, res) {
   });
 });
 
-// generating recommendations using Spotify API 
-app.get('/recommendation', function (req, res) {
-  // assume list of playlist track objects
-  // need: track id, artist id 
-  var playlist = req.body.playlist;
-  // TODO: verify valid playlist
-  // TODO: get playlist tracks + artists
-  var finPlaylist = [];
-  var partitions = [];
-  var currPartition = new Set();
 
-  // tracks + artist paritions
-  var spacing = 5;
-  for (var i = 0; i < playlist.length; i += spacing) {
-    partitions[partitions.length] = playlist.slice(i, i + spacing);
-  }
-
-  // pass into spotify web api
-  /*
-    npm install --save spotify-web-api-node axios url cors
-    
-    var router = express.Router();
-
-
-    var SpotifyWebApi = require('spotify-web-api-node');
-    scopes = ['user-read-private', 'user-read-email','playlist-modify-public','playlist-modify-private']
-
-    require('dotenv').config();
-
-    var spotifyApi = new SpotifyWebApi({
-      clientId: process.env.SPOTIFY_API_ID,
-      clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-      redirectUri: process.env.CALLBACK_URL,
-    });
-
-    # get user playlists
-    app.get('/playlists', async (req,res) => {
-    try {
-      var result = await spotifyApi.getUserPlaylists();
-      console.log(result.body);
-      res.status(200).send(result.body);
-    } catch (err) {
-      res.status(400).send(err)
-    }
-    # get recommendations
-    router.get('/recommendations', async (req, res) => {
-      var recs = await spotifyAPI.getRecommendations(options)
-      .then(result => {
-        res.status(200).send(result.body);
-      })
-      .catch(err => {
-        res.status(400).send({error: err})
-      })
-    }
-
-});
-  */
-  var endpoint = "https://api.spotify.com/v1/recommendations/available-genre-seeds"
-
-
-  /*
-  curl -X "GET" “URL” 
--H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer auth_token”
-
--H are headers
-URL is the link we have to build 
-sample: https://api.spotify.com/v1/recommendations?limit=10&market=ES&seed_artists=4NHQUGzhtTLFvgF5SZesLK&seed_genres=classical%2Ccountry&seed_tracks=0c6xIDDpzE81m2q797ordA
-limit, market, seed_artists are query parameters; we will only use limit, seed_artists, and seed_tracks
-“GET” - make a get request 
-auth_token i the authentication token
-content-type is the type of content sent to server
-  */
-
-  // build the URL - taken from https://github.com/JMPerez/spotify-web-api-js/blob/81f1a77461e02f2f73a284f092de0a76bfa925d5/src/spotify-web-api.js#L58
-  var parseURL = function (url, param) {
-    var quotes = '';
-    for (var key in param) {
-      //if we defined a value for param
-      if (param.hasOwnProperty(key)) {
-        var value = param[key];
-        //building something like limit=10&
-        quotes += encodeURIComponent(key) + '=' + encodeURIComponent(value) + '&';
-      }
-    }
-    if (quotes.length > 0) {
-      quotes = quotes.substring(0, quotes.length - 1);
-      url = url + "?" + quotes;
-    }
-    return url;
-  }
-  // set headers
-  if (access_token) {
-    req.setRequestHeader('Authorization: Bearer ' + access_token)
-  }
-
-  //TODO - request the recommendation given the ID and header
-  // request data from spotify using URL and headers
-
-
-  // store response playlists
-  var currRec = [];
-
-
-  res.status(200).send({
-    "recommendations": finPlaylist
-  })
-})
 
 console.log('Listening on port 8888')
 app.listen(process.env.PORT || 8888)
