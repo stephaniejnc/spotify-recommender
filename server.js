75ee133dab86af923675b3165735a729db6274d3
stephanie <stephaniechen0630@gmail.com>
2020-08-30 23:07:49 -0600

    commit

--- server.js
+++ server.js
@@ -210,7 +210,6 @@ app.get('/playlist-tracks', function (req, res) {
   getTracks();
 
   function getTracks() {
-<<<<<<< HEAD
     console.log(playlist)
     console.log(token)
     var select_playlist;
@@ -221,8 +220,6 @@ app.get('/playlist-tracks', function (req, res) {
       select_playlist = playlist[1]
     }
 
-=======
->>>>>>> master
     var playlistOptions = {
       url: `https://api.spotify.com/v1/playlists/${select_playlist}`,
       headers: {
